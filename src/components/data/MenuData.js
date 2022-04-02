904fa2d869a8d95a2117ca450909d9dcb4d07422
Valdinei Rodrigues <valvsari@gmail.com>
2022-04-01 21:39:32 -0300

    first commit

---
+++ src/components/data/MenuData.js
@@ -0,0 +1,6 @@
+export const menuData = [
+  { title: "Sobre nós", link: "/about" },
+  { title: "Projetos", link: "/projects" },
+  { title: "Equipe", link: "/team" },
+  { title: "Contato", link: "/contact" },
+]
