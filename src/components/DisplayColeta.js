ed90472346313c657a60b50ed1aa23d7caec9b20
Valdinei Rodrigues <valvsari@gmail.com>
2022-05-22 18:28:21 -0300

    update thingsboard

--- src/components/DisplayColeta.js
+++ src/components/DisplayColeta.js
@@ -1,35 +1,56 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 var XMLHttpRequest = require("xhr2")
 
-function getData() {
-  var data = "a"
-  var url =
-    "https://demo.thingsboard.io:443/api/plugins/telemetry/DEVICE/b3f21300-b683-11ec-942a-51543ec1dbaf/values/timeseries?keys=temperatura%2Cumidade"
-
-  var xhr = new XMLHttpRequest()
-  xhr.open("GET", url)
-
-  xhr.setRequestHeader("accept", "application/json")
-  xhr.setRequestHeader(
-    "X-Authorization",
-    "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ2YWx2c2FyaUBnbWFpbC5jb20iLCJzY29wZXMiOlsiVEVOQU5UX0FETUlOIl0sInVzZXJJZCI6IjUxMzk1ZmYwLWI2N2ItMTFlYy05NDJhLTUxNTQzZWMxZGJhZiIsImZpcnN0TmFtZSI6Ik1hbmt5IiwiZW5hYmxlZCI6dHJ1ZSwicHJpdmFjeVBvbGljeUFjY2VwdGVkIjp0cnVlLCJpc1B1YmxpYyI6ZmFsc2UsInRlbmFudElkIjoiNTA3YmI1NDAtYjY3Yi0xMWVjLTk0MmEtNTE1NDNlYzFkYmFmIiwiY3VzdG9tZXJJZCI6IjEzODE0MDAwLTFkZDItMTFiMi04MDgwLTgwODA4MDgwODA4MCIsImlzcyI6InRoaW5nc2JvYXJkLmlvIiwiaWF0IjoxNjUzMTY2NDE1LCJleHAiOjE2NTQ5NjY0MTV9.jnW8MaohFT84Q_dJayTblW7zXSkHseufM2MW5YyDQjJP8EP6eYr5vGUN3TfWvHYDchfChm7AWcq8BfSXJgFaeQ"
+function DisplayColeta() {
+  const [requestDone, setRequestDone] = useState(false)
+  const [data, setData] = useState(
+    '{"temperatura": [{"ts": 1653170715588,"value": 27.6 }]}'
   )
 
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-      //console.log(xhr.status)
-      console.log(JSON.parse(xhr.responseText).temperatura[0].value)
-      data = JSON.parse(xhr.responseText).temperatura[0].value
+  // Request da ultima telemetria de  temperatura e umidade guardada no thingsboard
+  useEffect(() => {
+    var url =
+      "https://demo.thingsboard.io:443/api/plugins/telemetry/DEVICE/b3f21300-b683-11ec-942a-51543ec1dbaf/values/timeseries?keys=temperatura%2Cumidade"
+
+    var xhr = new XMLHttpRequest()
+    xhr.open("GET", url)
+
+    xhr.setRequestHeader("accept", "application/json")
+    xhr.setRequestHeader(
+      "X-Authorization",
+      "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ2YWx2c2FyaUBnbWFpbC5jb20iLCJzY29wZXMiOlsiVEVOQU5UX0FETUlOIl0sInVzZXJJZCI6IjUxMzk1ZmYwLWI2N2ItMTFlYy05NDJhLTUxNTQzZWMxZGJhZiIsImZpcnN0TmFtZSI6Ik1hbmt5IiwiZW5hYmxlZCI6dHJ1ZSwicHJpdmFjeVBvbGljeUFjY2VwdGVkIjp0cnVlLCJpc1B1YmxpYyI6ZmFsc2UsInRlbmFudElkIjoiNTA3YmI1NDAtYjY3Yi0xMWVjLTk0MmEtNTE1NDNlYzFkYmFmIiwiY3VzdG9tZXJJZCI6IjEzODE0MDAwLTFkZDItMTFiMi04MDgwLTgwODA4MDgwODA4MCIsImlzcyI6InRoaW5nc2JvYXJkLmlvIiwiaWF0IjoxNjUzMTY2NDE1LCJleHAiOjE2NTQ5NjY0MTV9.jnW8MaohFT84Q_dJayTblW7zXSkHseufM2MW5YyDQjJP8EP6eYr5vGUN3TfWvHYDchfChm7AWcq8BfSXJgFaeQ"
+    )
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        setRequestDone(true)
+        //setData(JSON.parse(xhr.responseText).temperatura[0].ts)
+        setData(xhr.responseText)
+      }
     }
-  }
+    xhr.send()
+  })
 
-  xhr.send()
+  if (requestDone === true) {
+    const lastTs = JSON.parse(data).temperatura[0].ts
+    const lastDate = new Date(lastTs)
+    return (
+      <div>
+        {"Ultima telemetria: " +
+          lastDate.getHours() +
+          ":" +
+          lastDate.getMinutes() +
+          " de " +
+          lastDate.getUTCDay() +
+          "/" +
+          lastDate.getUTCMonth() +
+          "/" +
+          lastDate.getFullYear()}
+      </div>
+    )
+  } else return <div>carregando...</div>
 
-  return data
-}
-
-function DisplayColeta() {
-  return <div>{getData()}</div>
+  //return requestDone ? <div>{lastDate.getHours}</div> : <div>carregando...</div>
 }
 
 export default DisplayColeta
--- teste.js
+++ teste.js
@@ -1,22 +1,5 @@
-var XMLHttpRequest = require("xhr2")
+const ts = 1653170715588
 
-var url =
-  "https://demo.thingsboard.io:443/api/plugins/telemetry/DEVICE/b3f21300-b683-11ec-942a-51543ec1dbaf/values/timeseries?keys=temperatura%2Cumidade"
+const date = new Date(ts)
 
-var xhr = new XMLHttpRequest()
-xhr.open("GET", url)
-
-xhr.setRequestHeader("accept", "application/json")
-xhr.setRequestHeader(
-  "X-Authorization",
-  "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ2YWx2c2FyaUBnbWFpbC5jb20iLCJzY29wZXMiOlsiVEVOQU5UX0FETUlOIl0sInVzZXJJZCI6IjUxMzk1ZmYwLWI2N2ItMTFlYy05NDJhLTUxNTQzZWMxZGJhZiIsImZpcnN0TmFtZSI6Ik1hbmt5IiwiZW5hYmxlZCI6dHJ1ZSwicHJpdmFjeVBvbGljeUFjY2VwdGVkIjp0cnVlLCJpc1B1YmxpYyI6ZmFsc2UsInRlbmFudElkIjoiNTA3YmI1NDAtYjY3Yi0xMWVjLTk0MmEtNTE1NDNlYzFkYmFmIiwiY3VzdG9tZXJJZCI6IjEzODE0MDAwLTFkZDItMTFiMi04MDgwLTgwODA4MDgwODA4MCIsImlzcyI6InRoaW5nc2JvYXJkLmlvIiwiaWF0IjoxNjUzMTY2NDE1LCJleHAiOjE2NTQ5NjY0MTV9.jnW8MaohFT84Q_dJayTblW7zXSkHseufM2MW5YyDQjJP8EP6eYr5vGUN3TfWvHYDchfChm7AWcq8BfSXJgFaeQ"
-)
-
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4) {
-    //console.log(xhr.status)
-    console.log(JSON.parse(xhr.responseText).temperatura[0].value)
-  }
-}
-
-xhr.send()
+console.log(date.getSeconds())
